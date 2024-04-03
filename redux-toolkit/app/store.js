78a5b136602e439c4a1ef3b88837742990df434a
sumanjeet <sumanjeet0012@gmail.com>
2024-04-03 23:37:18 +0530

    logger added

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -1,12 +1,16 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const createLogger = require('redux-logger').createLogger;
+
+const logger = createLogger();
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store
