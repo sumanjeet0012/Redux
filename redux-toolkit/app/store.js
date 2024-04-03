5527c1c2a336b63c4135046bfb6ed40b8aa5e784
sumanjeet <sumanjeet0012@gmail.com>
2024-04-04 03:52:30 +0530

    createAsyncThunk added

--- redux-toolkit/app/store.js
+++ redux-toolkit/app/store.js
@@ -1,6 +1,7 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
 const cakeReducer = require('../features/cake/cakeSlice');
 const icecreamReducer = require('../features/icecream/icecreamSlice');
+const userReducer = require('../features/user/userSlice');
 const createLogger = require('redux-logger').createLogger;
 
 const logger = createLogger();
@@ -9,6 +10,7 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
+        user: userReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,17 +1,21 @@
 const store = require('./app/store');
 const cakeSlice = require('./features/cake/cakeSlice').cakeActions;
 const icecreamSlice = require('./features/icecream/icecreamSlice').icecreamSlice;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('Initial state:', store.getState());
-const unsubscribe = store.subscribe(() =>{}
-    // console.log('Updated state:', store.getState())
+const unsubscribe = store.subscribe(() =>
+    console.log('Updated state:', store.getState())
 );
-store.dispatch(cakeSlice.ordered());
-store.dispatch(cakeSlice.ordered());
-store.dispatch(cakeSlice.ordered());
 
-store.dispatch(cakeSlice.restocked(3));
-store.dispatch(icecreamSlice.ordered());
-store.dispatch(icecreamSlice.ordered());
-store.dispatch(icecreamSlice.restocked(2));
-unsubscribe();
+store.dispatch(fetchUsers())
+
+// store.dispatch(cakeSlice.ordered());
+// store.dispatch(cakeSlice.ordered());
+// store.dispatch(cakeSlice.ordered());
+
+// store.dispatch(cakeSlice.restocked(3));
+// store.dispatch(icecreamSlice.ordered());
+// store.dispatch(icecreamSlice.ordered());
+// store.dispatch(icecreamSlice.restocked(2));
+// unsubscribe();
